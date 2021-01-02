774304314286e8402f51969b7eebf69d216f20bd
abhisheksingh07 <abhishek.singh07@hotmail.com>
2021-01-03 01:39:37 +0530

    Update loadtest.js and conf file

--- loadtest.js
+++ loadtest.js
@@ -5,7 +5,7 @@ const https = require('https');
 let winston = require('winston');
 let SocksProxyAgent = require('socks-proxy-agent');
 const proxy = `socks://${conf.proxyIporHostname}:${conf.proxyPort}`;
-let request = url.parse(conf.requestUrl);
+let urlRequest = url.parse(conf.requestUrl);
 
 let loggers = {
   mjson: winston.createLogger({
@@ -19,75 +19,43 @@ let loggers = {
 // create an instance of the `SocksProxyAgent` class with the proxy server information
 let agent = new SocksProxyAgent(proxy);
 
-
-function sendRequest(requestID, callback) {
-  request.agent = agent;
-  for (let j = 1; j <= 5; j++) {
-    if (conf.https == true) {
-      https.get(request, (res) => {
-        let rawData = '';
-        res.on('data', (chunk) => { rawData += chunk; });
-        res.on('end', () => {
-          try {
-            loggers.mjson.log({
-              level: 'info',
-              message: `User count ${requestID} with request number ${j}`
-            })
-            callback(res, rawData)
-          } catch (e) {
-            console.error(e.message);
-          }
-        });
-      }).on('error', (error) => {
-        loggers.mjson.log({
-          level: 'error',
-          message: `SendRequest Error->${error.message}`
-        })
-      }).end();
-
-    } else {
-
-      http.get(request, async (res) => {
-        let rawData = '';
-        res.on('data', (chunk) => { rawData += chunk; });
-        res.on('end', () => {
-          try {
-            loggers.mjson.log({
-              level: 'info',
-              message: `User count ${requestID} with request number ${j}`
-            })
-            callback(res, rawData)
-          } catch (e) {
-            console.error(e.message);
-          }
-        });
-      }).on('error', (error) => {
-        loggers.mjson.log({
-          level: 'error',
-          message: `SendRequest Error->${error.message}`
-        })
-      }).end();
+function sendRequest(url, thread, requestNumber) {
+  return new Promise((resolve, reject) => {
+    if (conf.https) {
+      https.get(url, response => {
+        if (response.statusCode == 200) {
+          resolve(`Thread ${thread} - ${requestNumber} status code ${response.statusCode}`);
+        }
+        reject(`Error Thread ${thread} - ${requestNumber} status code ${response.statusCode}`)
+      }).on('error', (error) => (reject(`Error Thread ${thread} - ${requestNumber} Error status${error}`))).end()
     }
-  }
+    http.get(url, response => {
+      if (response.statusCode == 200) {
+        resolve(`Thread ${thread} - ${requestNumber} status code ${response.statusCode}`);
+      }
+      reject(`Error Thread ${thread} - ${requestNumber} status code ${response.statusCode}`)
+
+    }).on('error', (error) => (reject(`Error Thread ${thread} - ${requestNumber} Error status ${error}`))).end()
+  })
 }
 
-
-(function recursiveRequest(i) {
-  setTimeout(() => {
-    sendRequest(i, function (res) {
-      if (res.statusCode == "200") {
-        loggers.mjson.log({
-          level: 'info',
-          message: `Status Code ${res.statusCode}`
-        })
-      } else {
-        loggers.mjson.log({
-          level: 'error',
-          message: `Status Code ${res.statusCode}`
-        })
-      }
-    })
-    if (--i) recursiveRequest(i);
-  }, 1000)
-})(1000);
-
+async function threadRequest(thread) {
+  urlRequest.agent = agent;
+  for (let requestCount = 1; requestCount <= conf.loopCount; requestCount++) {
+    try {
+      let result = await sendRequest(urlRequest, thread, requestCount);
+      loggers.mjson.log({
+        level: 'info',
+        message: `${result}`
+      })
+    }
+    catch (error) {
+      loggers.mjson.log({
+        level: 'error',
+        message: `${error}`
+      })
+    }
+  }
+  if (--thread) threadRequest(thread);
+}
+threadRequest(conf.threadNumber);
